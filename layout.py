fcb8592505ce48cd77b2933671b190c27a9a0a05
SiddharthAgrawal2106 <siddharthagrawal.lpu@gmail.com>
2019-10-10 04:09:47 +0530

    Cleaned up paths and implementation

--- layout.py
+++ layout.py
@@ -7,7 +7,6 @@ Created on Wed Oct  2 23:09:54 2019
 from PIL import Image,ImageDraw
 from utils import get_filename_dict
 import sys
-from os.path import join as pathjoin
 import os
 
 def generate_layout(text, in_path, out_file):
@@ -21,6 +20,7 @@ def generate_layout(text, in_path, out_file):
     CHAR_SPACING    = 4
     SPACE_REPR      = '_space_'
     SPACE_WIDTH     = 75
+    EXP_FIX         = 54
 
     im = Image.new('RGB', (PAGE_WIDTH, PAGE_HEIGHT), PAGE_COLOR)
     """
@@ -30,70 +30,62 @@ def generate_layout(text, in_path, out_file):
         if i%LING_SPACING!=0:
             draw.line((0,i, 3400, i), fill=128, width=5)
     """
-    exp_lis = [pathjoin(in_path, 'roi/f_small.png'), 
-               pathjoin(in_path, 'roi/g_small.png'), 
-               pathjoin(in_path, 'roi/p_small.png'),
-               pathjoin(in_path, 'roi/y_small.png'),
-               pathjoin(in_path, 'roi/j_small.png'),
-               pathjoin(in_path, 'roi/q_small.png')]
+    exp_lis = ['f_small.png', 
+               'g_small.png', 
+               'p_small.png',
+               'y_small.png',
+               'j_small.png',
+               'q_small.png']
 
-    list_im = []
+    file_list = []
     names = get_filename_dict()
     for i in text:
         if i != ' ':
-            list_im.append(pathjoin(in_path, "roi/", names.get(i, 'x_cap.png')))
+            file_list.append(names.get(i, '8.png'))
         else:
-            list_im.append(SPACE_REPR)
-
-    list_am = []
-    for i in list_im:
-        list_am.append(i.replace(os.sep, '/'))
-
-    list_cm = []
+            file_list.append(SPACE_REPR)
     tot = 0
     ind = 0
     ind_n = 0
     tot_curr = 0
 
-    for i in list_am:
+    for i in file_list:
         if i == SPACE_REPR:
             ind = ind_n
             tot_curr = tot
             tot += SPACE_WIDTH
-            list_cm.append(i)
             if tot > (PAGE_WIDTH - MARGIN_LEFT - MARGIN_RIGHT):
-                list_am[ind] ="line_break"
+                file_list[ind] ="line_break"
                 tot = tot - tot_curr
         else:
-            im_c = Image.open(i)
-            w,h = im_c.size
+            im_c = Image.open(os.path.normpath(os.path.join(in_path, 'roi', i)))
+            w, h = im_c.size
             tot += w + CHAR_SPACING
-            list_cm.append(i)
             print(str(tot)+"\n")
             if tot > (PAGE_WIDTH - MARGIN_LEFT - MARGIN_RIGHT):
                 tot = tot-tot_curr
-                list_am[ind] = "line_break"
+                file_list[ind] = "line_break"
         ind_n += 1
         
-    print(list_am)
+    print(file_list)
     w_p = MARGIN_LEFT
     h_p = MARGIN_TOP
-    for img_name in list_am:    
+    for img_name in file_list:    
         if img_name == "line_break":
             w_p = MARGIN_LEFT
             h_p += LING_SPACING
         elif img_name == SPACE_REPR:
             w_p = w_p + SPACE_WIDTH
         else:
-            im_c = Image.open(img_name)
+            im_c = Image.open(os.path.normpath(os.path.join(in_path, 'roi', img_name)))
             if img_name in exp_lis:
                 w,h = im_c.size
-                im.paste(im_c,(w_p, h_p-54))
+                im.paste(im_c,(w_p, h_p-EXP_FIX))
             else:
                 w,h = im_c.size   
                 im.paste(im_c,(w_p,h_p-h))
-            w_p = w_p + w+4;
-            im.save(pathjoin(out_file))
+            w_p = w_p + w + CHAR_SPACING;
+    im.save(os.path.normpath(out_file))
 
 if __name__ == '__main__':
     text = 'Team Fractals 1234'
