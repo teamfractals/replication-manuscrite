a4d2c2bc0f19daabc840b696e0cd2562fe0a18b9
Aditya Kumar <kumar.sava.aditya@gmail.com>
2019-11-07 00:14:40 +0530

    Can use plain text file for console_test; Add requirements.txt

--- console_test.py
+++ console_test.py
@@ -3,6 +3,11 @@ from extract_roi import extract_roi_for_dir
 from layout import generate_layout
 import cv2
 import os
+import sys
+
+infile = None
+if len(sys.argv) > 1:
+    infile = sys.argv[1]
 
 getinput = input('Do you want to process a new image? (y/n) ')
 out_path = 'ERROR'
@@ -15,7 +20,11 @@ if getinput.lower() == 'y':
     extract_roi_for_dir(out_path)
 else: out_path = input('Output path to use instead: ')
 
-text = input('Text to print out:\n')
-generate_layout(text, out_path, 'out/__generated__')
+if infile is None:
+    text = input('Text to print out:\n')
+else:
+    with open(infile, 'r') as f:
+        text = f.read()
 
+generate_layout(text, out_path, 'out/__generated__')
 print('DONE')
